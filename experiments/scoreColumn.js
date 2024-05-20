ed2dece266421b27a4702da73718095be68ebb08
adlerweb <git@adlerweb.info>
2024-05-20 14:49:37 +0200

    Initial supernova column support

--- experiments/scoreColumn.js
+++ experiments/scoreColumn.js
@@ -1,20 +1,10 @@
-/* global ChromeUtils, libCommon, libExperiments, globalThis */
-/* exported scoreColumn */
-
-"use strict";
-
-/* eslint-disable no-var */
 var {ExtensionCommon} = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 var {ExtensionSupport} = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
+const { ThreadPaneColumns } = ChromeUtils.importESModule("chrome://messenger/content/thread-pane-columns.mjs");
 var Services = globalThis.Services || ChromeUtils.import("resource://gre/modules/Services.jsm").Services;
 var [majorVersion] = Services.appinfo.platformVersion.split(".", 1);
-/* eslint-enable no-var */
 
-const RspamdSpamnessColumn = {};
-
-// eslint-disable-next-line no-var
 var scoreColumn = class extends ExtensionCommon.ExtensionAPI {
-    // eslint-disable-next-line class-methods-use-this
     onShutdown(isAppShutdown) {
         if (isAppShutdown) return;
 
@@ -28,8 +18,6 @@ var scoreColumn = class extends ExtensionCommon.ExtensionAPI {
     getAPI(context) {
         const localStorage = {};
 
-        const window = Services.wm.getMostRecentWindow("mail:3pane");
-
         const {ExtensionParent} =
             ChromeUtils.import("resource://gre/modules/ExtensionParent.jsm");
         const extension = ExtensionParent.GlobalManager
@@ -37,115 +25,45 @@ var scoreColumn = class extends ExtensionCommon.ExtensionAPI {
         Services.scriptloader.loadSubScript(extension.getURL("scripts/libCommon.js"));
         Services.scriptloader.loadSubScript(extension.getURL("experiments/libExperiments.js"));
 
-        RspamdSpamnessColumn.handler = {
-            getCellProperties: function () {
-                // Do nothing.
-            },
-            getCellText: function (row) {
-                if (localStorage["display-column"] === "image")
-                    return null;
-                const score = RspamdSpamnessColumn.getScoreByRow(row);
-                return (isNaN(score)) ? "" : score.toFixed(2);
-            },
-            getImageSrc: function (row) {
-                if (localStorage["display-column"] === "text")
-                    return null;
-                const score = RspamdSpamnessColumn.getScoreByRow(row);
-                if (localStorage["display-columnImageOnlyForPositive"] && score <= 0)
-                    return null;
-                return extension.getURL(libCommon.getImageSrc(score));
-            },
-            getRowProperties: function () {
-                // Do nothing.
-            },
-            getSortLongForRow: function (hdr) {
-                return libCommon.getScoreByHdr(hdr, localStorage.header, true) * 1e4 + 1e8;
-            },
-            getSortStringForRow: function () {
-                return null;
-            },
-            isString: function () {
-                return false;
-            }
-        };
-
-        RspamdSpamnessColumn.getScoreByRow = function (row) {
-            return libCommon.getScoreByHdr(window.gDBView.getMsgHdrAt(row), localStorage.header, true);
-        };
-
-        RspamdSpamnessColumn.dbObserver = {
-            observe: function () {
-                RspamdSpamnessColumn.addColumnHandler();
-            }
-        };
-
-        RspamdSpamnessColumn.addColumnHandler = function () {
-            window.gDBView.addColumnHandler("spamScoreCol", RspamdSpamnessColumn.handler);
-        };
-
         context.callOnClose(this);
         return {
             scoreColumn: {
                 getCharPref(prefName) {
                     return Services.prefs.getCharPref(prefName);
                 },
-                init() {
-                    if (majorVersion > 110) return;
+                async init()    {
+                    const columnId = "spamScoreCol";
 
-                    // Listen for the main Thunderbird windows opening.
-                    ExtensionSupport.registerWindowListener("scoreColumnListener", {
-                        chromeURLs: ["chrome://messenger/content/messenger.xhtml"],
-                        onLoadWindow({document}) {
-                            (function loadCSS() {
-                                const href = extension.rootURI.resolve("experiments/scoreColumn.css");
+                    function getScore(row) {
+                        if (localStorage["display-column"] === "image")
+                            return null;
+                        const score = libCommon.getScoreByHdr(row, localStorage.header, true);
+                        return (isNaN(score)) ? "" : score.toFixed(2);
+                    }
 
-                                const link = document.createElement("link");
-                                link.id = "rspamd-spamness-messenger-css";
-                                link.rel = "stylesheet";
-                                link.href = href;
+                    function getImage(row) {
+                        if (localStorage["display-column"] === "text")
+                            return null;
+                        const score = libCommon.getScoreByHdr(row, localStorage.header, true);
+                        if (localStorage["display-columnImageOnlyForPositive"] && score <= 0)
+                            return null;
+                        //Still broken
+                        return extension.getURL(libCommon.getImageSrc(score));
+                    }
 
-                                const referenceNode = document.getElementById("tabmail-container");
-                                referenceNode.parentNode.insertBefore(link, referenceNode.previousSibling);
-                            }());
-                            (function addColumn() {
-                                const columnId = "spamScoreCol";
-                                if (document.getElementById(columnId)) return;
-
-                                const treeCol = document.createXULElement("treecol");
-                                treeCol.setAttribute("id", columnId);
-                                treeCol.setAttribute("persist", "hidden ordinal sortDirection width");
-                                treeCol.classList.add("headerValueBox");
-                                treeCol.setAttribute("label", context.extension.localeData
-                                    .localizeMessage("spamnessColumn.label"));
-                                treeCol.setAttribute("tooltiptext", context.extension.localeData
-                                    .localizeMessage("spamnessColumnToolTip.label"));
-                                treeCol.setAttribute("width", "60px");
-
-                                const threadCols = document.getElementById("threadCols");
-                                threadCols.appendChild(treeCol);
-
-                                // Restore persistent attributes. TB bug 1607575 and 1612055.
-                                const attributes = Services.xulStore.getAttributeEnumerator(document.URL, columnId);
-                                for (const attribute of attributes) {
-                                    const value = Services.xulStore.getValue(document.URL, columnId, attribute);
-                                    if (attribute === "ordinal") {
-                                        treeCol.ordinal = value;
-                                    } else {
-                                        treeCol.setAttribute(attribute, value);
-                                    }
-                                }
-                            }());
-
-                            Services.obs.addObserver(RspamdSpamnessColumn.dbObserver, "MsgCreateDBView", false);
-
-                            /*
-                             * After add-on installation the observer misses the first notification
-                             * since the window is already loaded.
-                             */
-                            Services.obs.notifyObservers(null, "MsgCreateDBView");
+                    ThreadPaneColumns.addCustomColumn(
+                        columnId, {
+                            name: context.extension.localeData.localizeMessage("spamnessColumn.label"),
+                            hidden: false,
+                            icon: false,
+                            resizable: true,
+                            sortable: true,
+                            textCallback: getScore,
+                            iconCallback: getImage
                         }
-                    });
+                    );
                 },
+
                 savePrefFile() {
                     Services.prefs.savePrefFile(null);
                 },
@@ -162,15 +80,8 @@ var scoreColumn = class extends ExtensionCommon.ExtensionAPI {
         };
     }
 
-    // eslint-disable-next-line class-methods-use-this
     close() {
-        if (majorVersion > 110) return;
-
-        libExperiments.removeElements([
-            "spamScoreCol",
-            "rspamd-spamness-messenger-css"
-        ]);
-        Services.obs.removeObserver(RspamdSpamnessColumn.dbObserver, "MsgCreateDBView", false);
-        ExtensionSupport.unregisterWindowListener("scoreColumnListener");
+        const columnId = "spamScoreCol";
+        ThreadPaneColumns.removeCustomColumn(columnId);
     }
 };
